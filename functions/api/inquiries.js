29079aec249b19882fb73321d7493a9c186b175b
MindfulwayTP <ryne@mindfulway-therapy.com>
2026-03-13 16:19:19 -0700

    fix pages

--- functions/api/inquiries.js
+++ functions/api/inquiries.js
@@ -1,19 +1,31 @@
 const APPS_SCRIPT_URL =
-  "https://script.google.com/macros/s/YAKfycbynKe5GKkqUs-nKq9JRlBMJ0TrltZtrSrkt0f4z8QIUfMmdau14kkdct_3b-kK67YE/exec";
+  "https://script.google.com/macros/s/AKfycbynKe5GKkqUs-nKq9JRlBMJ0TrltZtrSrkt0f4z8QIUfMmdau14kkdct_3b-kK67YE/exec";
 
 export async function onRequest(context) {
-  const url = new URL(context.request.url);
+  try {
+    const url = new URL(context.request.url);
+    const action = url.searchParams.get("action");
 
-  const action = url.searchParams.get("action");
+    const response = await fetch(`${APPS_SCRIPT_URL}?action=${action}`);
 
-  const response = await fetch(`${APPS_SCRIPT_URL}?action=${action}`);
+    const data = await response.text();
 
-  const data = await response.json();
-
-  return new Response(JSON.stringify(data), {
-    headers: {
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*",
-    },
-  });
+    return new Response(data, {
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+    });
+  } catch (err) {
+    return new Response(
+      JSON.stringify({
+        error: "Function crashed",
+        message: err.message,
+      }),
+      {
+        status: 500,
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+  }
 }
